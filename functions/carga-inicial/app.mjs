7cc97ea71672ad49f32d5fdef5446f7a65e09ad8
Andrew Maia <andrewsmaia@gmail.com>
2023-10-11 13:18:12 -0300

    ajustes

--- functions/carga-inicial/app.mjs
+++ functions/carga-inicial/app.mjs
@@ -7,53 +7,108 @@ const docClient = DynamoDBDocumentClient.from(client);
 const planoTable = process.env.PlanoTable;
 
 export const lambdaHandler = async (event, context) => {
+  console.log("REQUEST RECEIVED:\n" + JSON.stringify(event));
   try{
-    await waitUntilTableExists({client: client, maxWaitTime: 120}, {TableName: planoTable})
 
-    const command = new BatchWriteCommand({
-        RequestItems: {
-          [planoTable]: [
-            {
-                PutRequest: { 
-                    Item: { 
-                        nome: "Plano Bronze",
-                        valor: "17,99",
-                        centralOesteDesconto: 0,
-                        centralLesteDesconto: 0,
-                        golNorteDesconto: 0,
-                        golSulDesconto: 0,
-                        superiorDesconto: 20
-                    }
-                } 
-             },
-             {
-                PutRequest: { 
-                    Item: { 
-                        nome: "Plano Prata",
-                        valor: "41,99",
-                        centralOesteDesconto: 0,
-                        centralLesteDesconto: 25,
-                        golNorteDesconto: 50,
-                        golSulDesconto: 50,
-                        superiorDesconto: 50
-                    }
-                 }
-            }            
-          ],
-        },
-    });
-  
-    await docClient.send(command);
-    const responseData = { Result: 'Items added successfully' };
-    await response.send(event, context, response.SUCCESS, responseData);
+    sendResponse(event,context,"SUCCESS");
 
   } catch (error) {
-    console.log(error)
-    const responseData = { Error: 'Something went wrong' };
-    await response.send(event, context, response.FAILED, responseData);
+    console.log(error);
+    sendResponse(event,context,"FAILED");
   }
 };
 
+// Send response to the pre-signed S3 URL
+function sendResponse(event, context,responseStatus) {
+  var responseBody = JSON.stringify({
+    Status: responseStatus,
+    Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
+    PhysicalResourceId: context.logStreamName,
+    StackId: event.StackId,
+    RequestId: event.RequestId,
+    LogicalResourceId: event.LogicalResourceId,
+    Data: responseStatus
+  });
+
+  console.log("RESPONSE BODY:\n", responseBody);
+
+  var https = require("https");
+  var url = require("url");
+
+  var parsedUrl = url.parse(event.ResponseURL);
+  var options = {
+    hostname: parsedUrl.hostname,
+    port: 443,
+    path: parsedUrl.path,
+    method: "PUT",
+    headers: {
+        "content-type": "",
+        "content-length": responseBody.length
+    }
+  };
+
+  console.log("SENDING RESPONSE...\n");
+
+  var request = https.request(options, function(response) {
+    console.log("STATUS: " + response.statusCode);
+    console.log("HEADERS: " + JSON.stringify(response.headers));
+    // Tell AWS Lambda that the function execution is done  
+    context.done();
+  });
+
+  request.on("error", function(error) {
+    console.log("sendResponse Error:" + error);
+    // Tell AWS Lambda that the function execution is done  
+    context.done();
+  });
+
+  // write data to request body
+  request.write(responseBody);
+  request.end();
+}
+
+
+
+async function carregarPlanos(){
+  await waitUntilTableExists({client: client, maxWaitTime: 120}, {TableName: planoTable})
+
+  const command = new BatchWriteCommand({
+      RequestItems: {
+        [planoTable]: [
+          {
+              PutRequest: { 
+                  Item: { 
+                      id:"1",
+                      nome: "Plano Bronze",
+                      valor: "17,99",
+                      centralOesteDesconto: 0,
+                      centralLesteDesconto: 0,
+                      golNorteDesconto: 0,
+                      golSulDesconto: 0,
+                      superiorDesconto: 20
+                  }
+              } 
+           },
+           {
+              PutRequest: { 
+                  Item: { 
+                      id:"2",                      
+                      nome: "Plano Prata",
+                      valor: "41,99",
+                      centralOesteDesconto: 0,
+                      centralLesteDesconto: 25,
+                      golNorteDesconto: 50,
+                      golSulDesconto: 50,
+                      superiorDesconto: 50
+                  }
+               }
+          }            
+        ],
+      },
+  });
+
+  await docClient.send(command);
+}
 
 
 
