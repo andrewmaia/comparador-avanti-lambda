cc01eafabe1424e7cd7ca18ba5348ebd69d48912
Andrew Maia <andrewsmaia@gmail.com>
2023-10-19 09:59:09 -0300

    Refatoração function comparacao

--- functions/comparar/app.mjs
+++ functions/comparar/app.mjs
@@ -9,182 +9,184 @@ const planoTable = process.env.PlanoTable;
 
 export const lambdaHandler = async (event, context) => {
   try {
-    const planosComparados = new Map();
     let planos = await obterPlanos();
     let jogos = await obterJogos();
-
-    //Sem plano
-    const planoAvulso = {
-      planoId: 0,
-      planoNome: "Sem Plano",
-      planoValor: 0,
-      valorMensalidadePeriodo: 0,
-      valorTotal: 0,
-      jogos: [],
-    };
-
-    let params = event.body.split("&");
-    params.forEach((keyAndValue) => {
-      let keyValueArray = keyAndValue.split("=");
-      let jogoId = keyValueArray[0];
-      let setor = keyValueArray[1];
-      if (setor !== "") {
-        let jogo = jogos.find((jogo) => {
-          return jogo.id == jogoId;
-        });
-
-        //Sem plano
-        let valorIngressoInteiro = 0;
-        switch (setor) {
-          case "gn":
-            valorIngressoInteiro = jogo.golNorteValor;
-            break;
-          case "gs":
-            valorIngressoInteiro = jogo.golSulValor;
-            break;
-          case "co":
-            valorIngressoInteiro = jogo.centralOesteValor;
-            break;
-          case "cl":
-            valorIngressoInteiro = jogo.centraLesteValor;
-            break;
-          case "sn":
-            valorIngressoInteiro = jogo.superiorNorteValor;
-            break;
-          case "ss":
-            valorIngressoInteiro = jogo.superiorSulValor;
-            break;
-          case "sl":
-            valorIngressoInteiro = jogo.superiorLesteValor;
-            break;
-          case "so":
-            valorIngressoInteiro = jogo.superiorOesteValor;
-            break;
-          default:
-            valorIngressoInteiro = 0;
-        }
-        planoAvulso.jogos.push({
-          jogoId: jogo.id,
-          jogoAdversario: jogo.adversario,
-          jogoData: jogo.dataJogo,
-          setorComprado: setor,
-          valorIngresso: valorIngressoInteiro,
-        });
-        //Sem plano
-
-        planos.forEach((plano) => {
-          let valorIngresso = 0;
-          switch (setor) {
-            case "gn":
-              valorIngresso =
-                jogo.golNorteValor -
-                jogo.golNorteValor * (plano.golNorteDesconto / 100);
-              break;
-            case "gs":
-              valorIngresso =
-                jogo.golSulValor -
-                jogo.golSulValor * (plano.golSulDesconto / 100);
-              break;
-            case "co":
-              valorIngresso =
-                jogo.centralOesteValor -
-                jogo.centralOesteValor * (plano.centralOesteDesconto / 100);
-              break;
-            case "cl":
-              valorIngresso =
-                jogo.centraLesteValor -
-                jogo.centraLesteValor * (plano.centralLesteDesconto / 100);
-              break;
-            case "sn":
-              valorIngresso =
-                jogo.superiorNorteValor -
-                jogo.superiorNorteValor * (plano.superiorDesconto / 100);
-              break;
-            case "ss":
-              valorIngresso =
-                jogo.superiorSulValor -
-                jogo.superiorSulValor * (plano.superiorDesconto / 100);
-              break;
-            case "sl":
-              valorIngresso =
-                jogo.superiorLesteValor -
-                jogo.superiorLesteValor * (plano.superiorDesconto / 100);
-              break;
-            case "so":
-              valorIngresso =
-                jogo.superiorOesteValor -
-                jogo.superiorOesteValor * (plano.superiorDesconto / 100);
-              break;
-            default:
-              valorIngresso = 0;
-          }
-
-          if (!planosComparados.has(plano.id)) {
-            planosComparados.set(plano.id, {
-              planoId: plano.id,
-              planoNome: plano.nome,
-              planoValor: plano.valor,
-              valorMensalidadePeriodo: 0,
-              valorTotal: 0,
-              jogos: [],
-            });
-          }
-
-          const planoComparado = planosComparados.get(plano.id);
-          planoComparado.jogos.push({
-            jogoId: jogo.id,
-            jogoAdversario: jogo.adversario,
-            jogoData: jogo.dataJogo,
-            setorComprado: setor,
-            valorIngresso: valorIngresso,
-          });
-
-          //console.log(`Plano:${plano.nome}. Valor ingresso:${valorIngresso}`);
-        });
-      }
-    });
-
-    const p = [];
-    planosComparados.forEach(function (plano, planoId) {
-      plano.jogos.sort((a, b) => {
-        return new Date(a.jogoData) - new Date(b.jogoData);
-      });
-
-      const jogoMaisAntigo = plano.jogos[0];
-      const jogoMaisRecente = plano.jogos[plano.jogos.length - 1];
-
-      const meses = monthDiff(
-        jogoMaisAntigo.jogoData,
-        jogoMaisRecente.jogoData
-      );
-      plano.valorMensalidadePeriodo = plano.planoValor * meses;
-      const valorJogos = plano.jogos.reduce((total, jogo) => {
-        return { valorIngresso: total.valorIngresso + jogo.valorIngresso };
-      }).valorIngresso;
-      plano.valorTotal = plano.valorMensalidadePeriodo + valorJogos;
-      p.push(plano);
-    });
-
-    //Sem plano
-    const valorJogos = planoAvulso.jogos.reduce((total, jogo) => {
-      return { valorIngresso: total.valorIngresso + jogo.valorIngresso };
-    }).valorIngresso;
-    planoAvulso.valorTotal = valorJogos;
-    p.push(planoAvulso);
-
+    const planosComparados = await comparar(event.body, planos, jogos);
+    console.log(JSON.stringify(planosComparados));
     return {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
         "content-type": "application/json",
       },
-      body: JSON.stringify(p),
+      body: JSON.stringify(planosComparados),
     };
   } catch (err) {
     console.log(err);
     return err;
   }
 };
+
+async function comparar(body, planos, jogos) {
+  const planosComparados = new Map();
+  //Sem plano
+  const planoAvulso = {
+    planoId: 0,
+    planoNome: "Sem Plano",
+    planoValor: 0,
+    valorMensalidadePeriodo: 0,
+    valorTotal: 0,
+    jogos: [],
+  };
+
+  let params = body.split("&");
+  params.forEach((keyAndValue) => {
+    let keyValueArray = keyAndValue.split("=");
+    let jogoId = keyValueArray[0];
+    let setor = keyValueArray[1];
+    if (setor !== "") {
+      let jogo = jogos.find((jogo) => {
+        return jogo.id == jogoId;
+      });
+
+      //Sem plano
+      let valorIngressoInteiro = 0;
+      switch (setor) {
+        case "gn":
+          valorIngressoInteiro = jogo.golNorteValor;
+          break;
+        case "gs":
+          valorIngressoInteiro = jogo.golSulValor;
+          break;
+        case "co":
+          valorIngressoInteiro = jogo.centralOesteValor;
+          break;
+        case "cl":
+          valorIngressoInteiro = jogo.centraLesteValor;
+          break;
+        case "sn":
+          valorIngressoInteiro = jogo.superiorNorteValor;
+          break;
+        case "ss":
+          valorIngressoInteiro = jogo.superiorSulValor;
+          break;
+        case "sl":
+          valorIngressoInteiro = jogo.superiorLesteValor;
+          break;
+        case "so":
+          valorIngressoInteiro = jogo.superiorOesteValor;
+          break;
+        default:
+          valorIngressoInteiro = 0;
+      }
+      planoAvulso.jogos.push({
+        jogoId: jogo.id,
+        jogoAdversario: jogo.adversario,
+        jogoData: jogo.dataJogo,
+        setorComprado: setor,
+        valorIngresso: valorIngressoInteiro,
+      });
+      //Sem plano
+
+      planos.forEach((plano) => {
+        let valorIngresso = 0;
+        switch (setor) {
+          case "gn":
+            valorIngresso =
+              jogo.golNorteValor -
+              jogo.golNorteValor * (plano.golNorteDesconto / 100);
+            break;
+          case "gs":
+            valorIngresso =
+              jogo.golSulValor -
+              jogo.golSulValor * (plano.golSulDesconto / 100);
+            break;
+          case "co":
+            valorIngresso =
+              jogo.centralOesteValor -
+              jogo.centralOesteValor * (plano.centralOesteDesconto / 100);
+            break;
+          case "cl":
+            valorIngresso =
+              jogo.centraLesteValor -
+              jogo.centraLesteValor * (plano.centralLesteDesconto / 100);
+            break;
+          case "sn":
+            valorIngresso =
+              jogo.superiorNorteValor -
+              jogo.superiorNorteValor * (plano.superiorDesconto / 100);
+            break;
+          case "ss":
+            valorIngresso =
+              jogo.superiorSulValor -
+              jogo.superiorSulValor * (plano.superiorDesconto / 100);
+            break;
+          case "sl":
+            valorIngresso =
+              jogo.superiorLesteValor -
+              jogo.superiorLesteValor * (plano.superiorDesconto / 100);
+            break;
+          case "so":
+            valorIngresso =
+              jogo.superiorOesteValor -
+              jogo.superiorOesteValor * (plano.superiorDesconto / 100);
+            break;
+          default:
+            valorIngresso = 0;
+        }
+
+        if (!planosComparados.has(plano.id)) {
+          planosComparados.set(plano.id, {
+            planoId: plano.id,
+            planoNome: plano.nome,
+            planoValor: plano.valor,
+            valorMensalidadePeriodo: 0,
+            valorTotal: 0,
+            jogos: [],
+          });
+        }
+
+        const planoComparado = planosComparados.get(plano.id);
+        planoComparado.jogos.push({
+          jogoId: jogo.id,
+          jogoAdversario: jogo.adversario,
+          jogoData: jogo.dataJogo,
+          setorComprado: setor,
+          valorIngresso: valorIngresso,
+        });
+
+        //console.log(`Plano:${plano.nome}. Valor ingresso:${valorIngresso}`);
+      });
+    }
+  });
+
+  const p = [];
+  planosComparados.forEach(function (plano, planoId) {
+    plano.jogos.sort((a, b) => {
+      return new Date(a.jogoData) - new Date(b.jogoData);
+    });
+
+    const jogoMaisAntigo = plano.jogos[0];
+    const jogoMaisRecente = plano.jogos[plano.jogos.length - 1];
+
+    const meses = monthDiff(jogoMaisAntigo.jogoData, jogoMaisRecente.jogoData);
+    plano.valorMensalidadePeriodo = plano.planoValor * meses;
+    const valorJogos = plano.jogos.reduce((total, jogo) => {
+      return { valorIngresso: total.valorIngresso + jogo.valorIngresso };
+    }).valorIngresso;
+    plano.valorTotal = plano.valorMensalidadePeriodo + valorJogos;
+    p.push(plano);
+  });
+
+  //Sem plano
+  const valorJogos = planoAvulso.jogos.reduce((total, jogo) => {
+    return { valorIngresso: total.valorIngresso + jogo.valorIngresso };
+  }).valorIngresso;
+  planoAvulso.valorTotal = valorJogos;
+  p.push(planoAvulso);
+
+  return p;
+}
 
 async function obterPlanos() {
   const command = new ScanCommand({
